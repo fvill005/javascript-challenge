0a50195022d8b246881ba711c7ab1462d03c57b8
Filiberto Villegas <filibertovillegas92@gmail.com>
2020-09-10 20:42:32 -0700

    updated code, added comments and table working

--- app.js
+++ app.js
@@ -8,25 +8,30 @@ var tbody = d3.select("tbody");
 
 //for each loop to append cell into row and update with info from data.js
 //using arrow notation 
-data.forEach((sighting) => {
+function inserttable(data){
+  tbody.html("");
+  data.forEach((sighting) => {
   var row = tbody.append("tr");
   Object.entries(sighting).forEach(([key, value]) => {
     var cell = row.append("td");
     cell.text(value);
   });
 });
+}
+//builds table on index.html
+inserttable(tableData)
 
-var inputField = d3.select("datetime");
-var button = d3.select("fitler-btn");
-
-function datechange() {
-  d3.event.preventDefault();
-  console.log(inputField.property("value"));
-  var ufo_table = tableData.filter(sighting => sighting.datetime === inputField.property("value"))
-  displayData(ufo_table)
-};
-
-inputField.on("change", datechange);
-button.on("click", datechange);
-
-        
+//filters data fot input date
+function filterData() {
+  var date = d3.select("#datetime").property("value");
+  //console.log(date)
+  var filteredData = tableData;
+  //console.log(filteredData)
+  if (date){
+      filteredData = filteredData.filter(row => row.datetime=== date);
+      console.log(filteredData)
+  }
+  //builds new table based on date input 
+inserttable(filteredData)
+}
+d3.selectAll("#filter-btn").on("click",filterData)
