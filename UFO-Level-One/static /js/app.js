259fbf57ce020e988328d6b0ad9bc66372789027
Filiberto Villegas <filibertovillegas92@gmail.com>
2020-09-10 21:01:56 -0700

    added folders and moved files in them

--- UFO-Level-One/static /js/app.js
+++ UFO-Level-One/static /js/app.js
@@ -1,46 +1,37 @@
 // from data.js
+//naming variable for info in data.js
 var tableData = data;
 console.log(tableData);
 
-data.forEach(function(sighting) {
-  console.log(sighting);
-});
+//declaring variable for tbody to append rows to index
+var tbody = d3.select("tbody");
 
-data.forEach(function(sighting) {
-      console.log(sighting);
-      var row = tbody.append("tr");
-    });
-    
-data.forEach(function(sighting) {
-  console.log(sighting);
+//for each loop to append cell into row and update with info from data.js
+//using arrow notation 
+function inserttable(data){
+  tbody.html("");
+  data.forEach((sighting) => {
   var row = tbody.append("tr");
-
-  Object.entries(sighting).forEach(function([key, value]) {
-    console.log(key, value);
-  });
-});
-
-data.forEach(function(sighting) {
-      console.log(sighting);
-      var row = tbody.append("tr");
-    
-      Object.entries(sighting).forEach(function([key, value]) {
-        console.log(key, value);
-        // Append a cell to the row for each value
-        // in the weather report object
-        var cell = row.append("td");
-      });
-    });
-    
-
-    data.forEach(function(sighting) {
-  console.log(sighting);
-  var row = tbody.append("tr");
-  Object.entries(sighting).forEach(function([key, value]) {
-    console.log(key, value);
-    // Append a cell to the row for each value
-    // in the weather report object
+  Object.entries(sighting).forEach(([key, value]) => {
     var cell = row.append("td");
     cell.text(value);
   });
 });
+}
+//builds table on index.html
+inserttable(tableData)
+
+//filters data fot input date
+function filterData() {
+  var date = d3.select("#datetime").property("value");
+  //console.log(date)
+  var filteredData = tableData;
+  //console.log(filteredData)
+  if (date){
+      filteredData = filteredData.filter(row => row.datetime=== date);
+      console.log(filteredData)
+  }
+  //builds new table based on date input 
+inserttable(filteredData)
+}
+d3.selectAll("#filter-btn").on("click",filterData)
